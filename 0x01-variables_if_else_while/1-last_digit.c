6f2b8e4596db6c049a18a93fa4a0e878504ed2c4
Ngang Ivo Nde <ngangivonde@gmail.com>
2023-02-17 17:34:01 -0800

    Task 1. compile and run check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	a = n % 10;
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d nd is greater than 5\n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (n == 0)
 	{
