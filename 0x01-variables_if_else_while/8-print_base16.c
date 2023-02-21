702ef102408d427dc31e6e4e4378f3fbfebac0a0
Ngang Ivo Nde <ngangivonde@gmail.com>
2023-02-20 18:13:18 -0800

    task 8. print hexadecimal numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print base 16 numbers in lowercase
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
