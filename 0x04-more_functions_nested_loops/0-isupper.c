f6b56438eeef28d0f91d0b9538f500ffa9972007
Ngang Ivo Nde <ngangivonde@gmail.com>
2023-02-23 16:36:34 -0800

    Task 0. Checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a letter is uppercase
+ * @x: the number to be checked
+*/
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
