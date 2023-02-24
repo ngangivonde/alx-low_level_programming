dfdf1c1daa9c7fe607035c47f201846524b048d1
Ngang Ivo Nde <ngangivonde@gmail.com>
2023-02-23 16:51:47 -0800

    Task 2. Multiplu two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiples two intergers
+ * @x: the first integer to be multiplied with,
+ * @y: the second interger to be multiplied
+ * Return: the result of multiplication of x param and y param
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
